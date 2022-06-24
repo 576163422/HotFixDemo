f115febdd7ef2fb1422b0ecd35a611461ef93136
tianyuhe3@cmcm.com <tianyuhe3@cmcm.com>
2022-06-24 16:05:51 +0800

    [修改]热修复插件3 增加字节码插桩问题理解，为什么要替换 class或者jar文件？ 因为CLASS_ISPREVERIFIED标志问题，如果不插桩其他Dex类的引用，就无法对这个类热修复进行热修复。所以打包apk时，需要把所有的类进行插桩，去除因为CLASS_ISPREVERIFIED标志

--- buildSrc/src/main/java/com/faytian/plugin/PatchPlugin.java
+++ buildSrc/src/main/java/com/faytian/plugin/PatchPlugin.java
@@ -8,6 +8,7 @@ import com.faytian.bean.HotFixExtBean;
 import com.faytian.utils.ClassUtil;
 import com.faytian.utils.PatchUtil;
 
+import org.apache.commons.compress.utils.IOUtils;
 import org.gradle.api.Action;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
@@ -18,6 +19,7 @@ import org.gradle.api.plugins.PluginContainer;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
@@ -26,6 +28,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
 import java.util.regex.Matcher;
 
 
@@ -154,7 +157,7 @@ public class PatchPlugin implements Plugin<Project> {
     }
 
     /**
-     *
+     * 获取其他模块的类时，只有app模块能直接拿到class，其他模块是以jar传递过来的，需要从jar文件中获取class
      * @param applicationName
      * @param file              jar文件
      * @param md5Map
@@ -163,45 +166,45 @@ public class PatchPlugin implements Plugin<Project> {
     private void processJar(String applicationName, File file, Map<String, String> md5Map, PatchGenerator patchGenerator) {
         try {
             applicationName = applicationName.replaceAll(Matcher.quoteReplacement(File.separator), "/");
-//            File bakJar = new File(file.getParent(), file.getName() + ".bak");//todo
-//            JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(bakJar));//todo
+            File bakJar = new File(file.getParent(), file.getName() + ".bak");
+            JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(bakJar));
             //解压jar文件
             JarFile jarFile = new JarFile(file);
             Enumeration<JarEntry> entries = jarFile.entries();
             while (entries.hasMoreElements()) {
                 JarEntry jarEntry = entries.nextElement();
 
-//                jarOutputStream.putNextEntry(new JarEntry(jarEntry.getName()));//todo
+                jarOutputStream.putNextEntry(new JarEntry(jarEntry.getName()));
                 InputStream is = jarFile.getInputStream(jarEntry);//todo 从jar文件中获取
                 String className = jarEntry.getName();
-                if (className.endsWith(".class") && !className.contains(applicationName)
-                        && !PatchUtil.isAndroidClass(className) && !className.startsWith("com/example/patchlib")) {//可以进行热修复的class
+                if (className.endsWith(".class") && !className.contains(applicationName)//可以进行热修复的class
+                        && !PatchUtil.isAndroidClass(className) && !className.startsWith("com/example/patchlib")) {
                     //进行插桩，插入单独dex的代码，解决 CLASS_ISPREVERIFIED标志导致的问题
                     byte[] byteCode = ClassUtil.referHackWhenInit(is);
                     //得到class的md5
                     String md5 = PatchUtil.hex(byteCode);
                     md5Map.put(className, md5);
                     patchGenerator.checkClass(className, md5, byteCode);
-//                    jarOutputStream.write(byteCode);//todo
+                    jarOutputStream.write(byteCode);
                 } else {
                     //无法热修复的class
-                    //输出到临时文件
-//                    jarOutputStream.write(IOUtils.toByteArray(is));//todo
+                    jarOutputStream.write(IOUtils.toByteArray(is));//输出到临时文件
                 }
                 is.close();
-//                jarOutputStream.closeEntry();//todo
+                jarOutputStream.closeEntry();
             }
-//            jarOutputStream.close();//todo
+            jarOutputStream.close();
             jarFile.close();
-//            file.delete();//todo
-//            bakJar.renameTo(file);//todo
+            file.delete();
+            //todo 使用插桩后的class生成新的jar，替换其他模块传递过来的jar，因为CLASS_ISPREVERIFIED标志问题，如果不插桩，就不能热修复这个类。所以打包apk时，需要所有的类进行插桩，去除因为CLASS_ISPREVERIFIED标志
+            bakJar.renameTo(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     *
+     * 获取app模块下的class，调用该方法
      * @param applicationName
      * @param file              class文件， HotFixDemo/app/build/intermediates/classes/debug/com/faytian/hotfixdemo/MainActivity.class
      * @param dirName
@@ -209,21 +212,26 @@ public class PatchPlugin implements Plugin<Project> {
      * @param patchGenerator
      */
     private void processClass(String applicationName, File file, String dirName, Map<String, String> md5Map, PatchGenerator patchGenerator) {
+        //去除目录名
+        String filePath = file.getAbsolutePath();
+        String s = filePath.split(dirName)[1];
+        String className = s.substring(1);
+        //application 和 android包下的类无法进行热修复，所以这里屏蔽掉
+        if (className.contains(applicationName) || PatchUtil.isAndroidClass(className)) {
+            return;
+        }
         try {
             //得到 class文件的输入流
-            FileInputStream fos = new FileInputStream(file);
+            FileInputStream fis = new FileInputStream(file);
             //进行插桩，插入单独dex的代码，解决 CLASS_ISPREVERIFIED标志导致的问题
-            byte[] byteCode = ClassUtil.referHackWhenInit(fos);
+            byte[] byteCode = ClassUtil.referHackWhenInit(fis);
             //得到class的md5
             String md5 = PatchUtil.hex(byteCode);
-            //去除目录名
-            String absolutePath = file.getAbsolutePath();
-            String s = absolutePath.split(dirName)[1];
-            String className = s.substring(1);
-            //application 和 android包下的类无法进行热修复，所以这里屏蔽掉
-            if (className.contains(applicationName) || PatchUtil.isAndroidClass(className)) {
-                return;
-            }
+            //todo 这里为什么要再把已插桩的class，在写入到file中，因为CLASS_ISPREVERIFIED标志问题，如果不插桩，就不能热修复这个类。所以打包apk时，需要所有的类进行插桩，去除因为CLASS_ISPREVERIFIED标志
+            FileOutputStream fos = new FileOutputStream(filePath);
+            fos.write(byteCode);
+            fos.close();
+
             md5Map.put(className, md5);
             patchGenerator.checkClass(className, md5, byteCode);
         } catch (Exception e) {
